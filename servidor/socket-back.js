c969f3057ace035052e72d0d1a440a56340e147f
Iago Engel Serafim <iagoengel@yahoo.com>
2023-11-08 16:06:02 -0300

    Módulo 3: Controlando acessos

--- servidor/socket-back.js
+++ servidor/socket-back.js
@@ -4,11 +4,20 @@ import registrarEventosCadastro from "./registrarEventos/cadastro.js";
 import registrarEventosDocumento from "./registrarEventos/documento.js";
 import registrarEventosInicio from "./registrarEventos/inicio.js";
 import registrarEventosLogin from "./registrarEventos/login.js";
-import io from "./servidor.js";
 
-io.on("connection", (socket) => {
+import io from "./servidor.js";
+import autorizarUsuario from "./middlewares/autorizarUsuario.js";
+
+const nspUsuarios = io.of("/usuarios"); // nsp é uma convenção para encurtar o nome namespace
+
+nspUsuarios.use(autorizarUsuario);
+
+nspUsuarios.on("connection", (socket) => {
+  registrarEventosInicio(socket, nspUsuarios)  ;
+  registrarEventosDocumento(socket, nspUsuarios);
+});
+
+io.of("/").on("connection", (socket) => {
   registrarEventosCadastro(socket, io);
   registrarEventosLogin(socket, io);
-  registrarEventosInicio(socket, io)  ;
-  registrarEventosDocumento(socket, io);
 });
